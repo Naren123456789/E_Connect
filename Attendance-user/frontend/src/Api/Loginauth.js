1af2c55fe6d5d16755364808d295bd6117103a32
Naren123456789 <stbnarenkishor@gmail.com>
2025-02-26 15:44:50 +0530

    update

---
+++ Attendance-user/frontend/src/Api/Loginauth.js
@@ -0,0 +1,51 @@
+import { comma } from "postcss/lib/list";
+import { Baseaxios, LS } from "../Utils/Resuse";
+import { toast } from "react-hot-toast";
+export const Apisignup = async ({ email, password, name }) => {
+  const response = await Baseaxios.post("/signup", {
+    email: email,
+    password: password,
+    name: name,
+  }).catch((err) => {
+    const a = err.response.data.detail;
+    toast.error(a);
+    throw new Error(a);
+  });
+
+  const Comp = response.data;
+  LS.save("userid", Comp.userid);
+  LS.save("name", Comp.name);
+  LS.save("email", Comp.email)
+  LS.save("access_token", Comp.access_token);
+  LS.save("id", Comp.id)
+  LS.save("Auth", true);
+  // LS.save("isManager",)
+
+  return Comp;
+};
+
+export const Apisignin = async ({ client_name, email }) => {
+  const response = await Baseaxios.post("/Gsignin", {
+    client_name: client_name,
+    email: email,
+  }).catch((err) => {
+    const a = err.response.data.detail;
+    toast.error(a);
+    throw new Error(a);
+  });
+  const Comp = response.data;
+  LS.save("name", Comp.name);
+  LS.save("email", Comp.email);
+  LS.save("userid", Comp.userid);
+  LS.save("position",Comp.position);
+  LS.save("department",Comp.department)
+  LS.save("access_token", Comp.access_token);
+  LS.save("id", Comp.id)
+  LS.save("Auth", true);
+  LS.save("isadmin", Comp.isadmin);
+  LS.save("isloggedin", Comp.isloggedin);
+  toast.success("Login Successfully");
+  return Comp;
+};
+
+
